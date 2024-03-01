ce1f9a6d03104f5a94096391b96b04d6e76eddf2
Samuel Othieno <douglasothieno@gmail.com>
2024-03-01 20:08:47 +0300

    Initial Commit: Add Node.JS & Express project

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const server= express();
+
+
+
+server.get('/', (req, res)=> {
+    res.send('Welcome to my sever.');
+})
+
+server.get('/home', (req, res)=> {
+    res.send('Welcome to the home page.');
+})
+
+server.get('/about', (req, res)=> {
+    res.send('This is the About page');
+})
+
+server.get('/Contact', (req, res)=> {
+    res.send('This is the Contact page');
+})
+
+server.listen(4000, ()=>{
+    console.log('Server listening on port 4000');
+})
